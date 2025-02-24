73ec601ce6ed248ac3b3a895c72963d4a4e52c05
Fuqiao Xue <xfq@w3.org>
2025-02-24 10:48:27 +0800

    Fix encoding error

--- footer.php
+++ footer.php
@@ -10,7 +10,7 @@
 	<hr />
 
 	<div class="smallprint" id="endMatter" style="font-size: 80%; text-align: center;"> 
-		<span class="copyright">Copyright Â© 2025 <a href="https://www.w3.org/">World Wide Web Consortium</a>.<br> <abbr title="World Wide Web Consortium">W3C</abbr><sup>Â®</sup> <a href="https://www.w3.org/policies/#disclaimers">liability</a>, <a href="https://www.w3.org/policies/#trademarks">trademark</a> and <a rel="license" href="https://www.w3.org/copyright/document-license/" title="W3C Document License">permissive license</a> rules apply.</span><br />
+		<span class="copyright">Copyright © 2025 <a href="https://www.w3.org/">World Wide Web Consortium</a>.<br> <abbr title="World Wide Web Consortium">W3C</abbr><sup>®</sup> <a href="https://www.w3.org/policies/#disclaimers">liability</a>, <a href="https://www.w3.org/policies/#trademarks">trademark</a> and <a rel="license" href="https://www.w3.org/copyright/document-license/" title="W3C Document License">permissive license</a> rules apply.</span><br />
         Questions or comments? <a href="mailto:xfq@w3.org">xfq@w3.org</a>
 	</div>
 
