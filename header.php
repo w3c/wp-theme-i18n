aeec01a5a658d1b1ba3d16dcf46c5da961928357
Jean-Guilhem Rouel <jean-gui@w3.org>
2025-02-28 16:11:26 +0100

    sidebar

--- header.php
+++ header.php
@@ -90,42 +90,43 @@ elseif (is_single())
 </div>
 
 <div class="directory rightCol">
+    <?php get_sidebar(); ?>
 
-<?php get_sidebar(); ?>
+    <div class="tocbox" style="padding-bottom: 10px;">
+        <h2 id="sec_site_links">Search for news</h2>
+        <div>
+            <div class="toc1">News by category</div>
+            <div class="bSideItem">
+                <?php
+                foreach ((get_categories()) as $category) {
+                    echo '<div class="toc2"><a href="', get_category_link($category->term_id), "\">\n";
+                    echo " ", $category->name, "</a>", ' <span class="notes">(', $category->count, ")</span></div>\n";
+                } ?>
+            </div>
+        </div>
+        <div>
+            <div class="toc1">News archives</div>
+            <div class="bSideItem">
+                <?php
+                wp_get_archives(
+                    'type=monthly&format=custom&before=<div class="toc2">&after=</div>&show_post_count=true'
+                ); ?>
+            </div>
+        </div>
+        <div>
+            <div class="toc1">Search news</div>
+            <div class="bSideItem" style="padding: 10px;">
+                <?php
+                get_search_form(); ?>
+            </div>
+        </div>
 
+    </div>
 
-<div class="tocbox" style="padding-bottom: 10px;">
-    <h2 id="sec_site_links">Search for news</h2>
-					<div>
-						<div class="toc1">News by category</div>
-						<div class="bSideItem">
-							<?php foreach ((get_categories()) as $category) {
-							  echo '<div class="toc2"><a href="', get_category_link($category->term_id), "\">\n";
-							  echo " ", $category->name, "</a>", ' <span class="notes">(', $category->count, ")</span></div>\n";
-							} ?>
-						</div>
-					</div>
-					<div>
-						<div class="toc1">News archives</div>
-						<div class="bSideItem">
-							<?php wp_get_archives( 'type=monthly&format=custom&before=<div class="toc2">&after=</div>&show_post_count=true' ); ?>
-						</div>
-					</div>
-					<div>
-						<div class="toc1">Search news</div>
-						<div class="bSideItem" style="padding: 10px;">
-							<?php get_search_form(); ?>
-						</div>
-					</div>
-
-  </div>
-
-
-
-<!--h2>Search for news</h2>
-  	< ?php get_search_form(); ? -->
-
-	<!--h2>Admin</h2-->
-
+	<ul style="font-size: 80%; margin-top: 3em;">
+		<?php wp_register(); ?>
+		<li><?php wp_loginout(); ?></li>
+        <li>SMedia icons by <a target="_blank" href="https://icons8.com">Icons8</a></li>
+	</ul>
 </div>
 
